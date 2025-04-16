98b873967e27efe2a3b0752800bf4d21ea6fb7a9
Aravind <aravind19112003@gmail.com>
2025-04-16 12:02:38 +0530

    Add endpoint for adding a showtime & getting all active showtimes

--- src/main/java/com/example/mock/controller/ShowTimeController.java
+++ src/main/java/com/example/mock/controller/ShowTimeController.java
@@ -1,9 +1,33 @@
 package com.example.mock.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.example.mock.dto.ShowTimeDTO;
+import com.example.mock.entity.ShowTime;
+import com.example.mock.service.showtime.ShowTimeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/showtime")
 public class ShowTimeController {
+
+    @Autowired
+    private ShowTimeService showTimeService;
+
+    @PostMapping("/add")
+    @PreAuthorize("hasRole('ADMIN')")
+    ResponseEntity<ShowTime> addShowTime(@RequestBody ShowTimeDTO showTimeDTO){
+        ShowTime result = showTimeService.addShowTime(showTimeDTO);
+        return ResponseEntity.status(HttpStatus.CREATED).body(result);
+    }
+
+    @GetMapping("/all")
+    ResponseEntity<List<ShowTime>> getAllShowTimes(){
+        List<ShowTime> result = showTimeService.getAllShowTimes();
+        return ResponseEntity.status(HttpStatus.FOUND).body(result);
+    }
 }
--- src/main/java/com/example/mock/service/showtime/ShowTimeService.java
+++ src/main/java/com/example/mock/service/showtime/ShowTimeService.java
@@ -1,16 +1,45 @@
 package com.example.mock.service.showtime;
 
 import com.example.mock.dto.ShowTimeDTO;
+import com.example.mock.entity.Movie;
+import com.example.mock.entity.Screen;
 import com.example.mock.entity.ShowTime;
+import com.example.mock.repo.ShowTimeRepository;
+import com.example.mock.service.movie.MovieService;
+import com.example.mock.service.screen.ScreenService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
 public class ShowTimeService implements IShowTime{
+
+    @Autowired
+    private MovieService movieService;
+
+    @Autowired
+    private ScreenService screenService;
+
+    @Autowired
+    private ShowTimeRepository showTimeRepository;
+
     @Override
     public ShowTime addShowTime(ShowTimeDTO showTimeDTO) {
-        return null;
+
+        ShowTime showTime = new ShowTime();
+        showTime.setStartTime(showTimeDTO.getStartTime());
+        showTime.setEndTime(showTimeDTO.getEndTime());
+        showTime.setPrice(showTimeDTO.getPrice());
+        showTime.setActive(showTime.isActive());
+
+        Movie movie = movieService.getById(showTimeDTO.getMovieId());
+        showTime.setMovie(movie);
+        Screen screen = screenService.getScreenById(showTimeDTO.getScreenId());
+        showTime.setScreen(screen);
+
+        return showTimeRepository.save(showTime);
+
     }
 
     @Override
@@ -30,7 +59,7 @@ public class ShowTimeService implements IShowTime{
 
     @Override
     public List<ShowTime> getAllShowTimes() {
-        return List.of();
+        return showTimeRepository.findAll();
     }
 
     @Override
